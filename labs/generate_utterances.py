e88c57a1ee8ad48ab0c32d218f82dce86e38e02c
Tahar <zanouda@gmail.com>
2018-03-22 00:43:15 +0100

    added how-to-use fountain lib script

--- labs/generate_utterances.py
+++ labs/generate_utterances.py
@@ -10,10 +10,21 @@ except:
 import logging
 if __name__ == "__main__":
     logging.info('Using `fountain` to generate data')
+    # DataGenerator
     data_generator = DataGenerator()
+    # load template
     template_fname = 'utterances_template.yaml'
     results = data_generator.parse(template_fname)
     utterances_csv = 'utterances.csv'
     data_generator.to_csv(utterances_csv)
     utterances_json = 'utterances.json'
     data_generator.to_json(utterances_json)
+
+    # case where you have builtin slot
+    template_fname = 'utterances_template_builtin.yaml'
+    results = data_generator.parse(template_fname)
+    utterances_csv = 'utterances_builtin.csv'
+    data_generator.to_csv(utterances_csv)
+    utterances_json = 'utterances_builtin.json'
+    data_generator.to_json(utterances_json)
+
